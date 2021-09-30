a359906a539ae76f015568ceb09556b99ef3ada5
sdcode999 <mbuz12ra@outlook.com>
2021-09-30 11:20:08 +0700

    add file app.js & make navbar fixed

---
+++ app.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header-nav");
+  header.classList.toggle("stick", window.scrollY > 0);
+});
+
+const navLink = document
+  .querySelector(".header-nav nav ul")
+  .querySelectorAll("a");
+
+navLink.forEach((Element) => {
+  Element.addEventListener("click", function () {
+    navLink.forEach((nav) => nav.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
